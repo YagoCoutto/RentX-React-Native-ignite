d75e3ef73530998bf83c4560a5eae5fbb19472f5
Yago <62577739+YagoCoutto@users.noreply.github.com>
2023-11-13 11:19:14 -0300

    Screen CarDetails

--- src/Screens/carDetails/index.tsx
+++ src/Screens/carDetails/index.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
-import { CarImages, Container, Header } from './styles'
+import {
+    CarImages,
+    Container,
+    Header,
+    Content,
+    Details,
+    Description,
+    Brand,
+    Name,
+    Rent,
+    Period,
+    Price,
+    About
+} from './styles'
 import BackButton from '../../components/BackButton'
 import ImageSlider from '../../components/ImageSlider'
 
@@ -16,6 +29,34 @@ export default function CarDetails() {
                     imageUrl={['https://static.wikia.nocookie.net/forzamotorsport/images/0/0f/Mot_mit_lancer_08.png/revision/latest?cb=20190531190611']}
                 />
             </CarImages>
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand>
+                            mitsubishi
+                        </Brand>
+                        <Name>
+                            Lancer EVO X
+                        </Name>
+                    </Description>
+                    <Rent>
+                        <Period>
+                            ao dia
+                        </Period>
+                        <Price>
+                            R$ 340
+                        </Price>
+                    </Rent>
+                </Details>
+                <About>
+                    Experimente a emoção da estrada com o Mitsubishi Lancer Evolution X!
+                    Este ícone esportivo combina potência e desempenho em um design
+                    aerodinâmico e agressivo. Com um motor turboalimentado, tração
+                    integral e um interior projetado para entusiastas, o EVO X oferece
+                    uma experiência de condução única. Descubra o equilíbrio perfeito
+                    entre estilo e velocidade neste carro lendário.
+                </About>
+            </Content>
         </Container>
     )
 }
--- src/Screens/carDetails/styles.ts
+++ src/Screens/carDetails/styles.ts
@@ -16,5 +16,59 @@ export const Header = styled.View`
     margin-left: 24px;
 `
 export const CarImages = styled.View`
-    margin-top: ${getStatusBarHeight()+32}px;
+    margin-top: ${getStatusBarHeight() + 32}px;
 `;
+
+export const Content = styled.ScrollView.attrs({
+    contentContainerStyle:{
+        padding: 24,
+        alignItems:'center',
+    }
+})``;
+
+export const Details = styled.View`
+    width: 100%;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+`;
+
+export const Description = styled.View``;
+
+export const Brand = styled.Text`
+    font-family: ${({theme}) => theme.fonts.ArchivoMedium};
+    color: ${({theme}) => theme.colors.subTitle};
+    font-size: ${RFValue(10)}px;
+    text-transform: uppercase;
+`;
+
+export const Name = styled.Text`
+    font-family: ${({theme}) => theme.fonts.ArchivoMedium};
+    color: ${({theme}) => theme.colors.title};
+    font-size: ${RFValue(25)}px;
+`;
+
+export const Rent = styled.View``;
+
+export const Period = styled.Text`
+    font-family: ${({theme}) => theme.fonts.ArchivoMedium};
+    color: ${({theme}) => theme.colors.subTitle};
+    font-size: ${RFValue(10)}px;
+    text-transform: uppercase;
+`;
+
+export const Price = styled.Text`
+    font-family: ${({theme}) => theme.fonts.ArchivoMedium};
+    color: ${({theme}) => theme.colors.primary};
+    font-size: ${RFValue(25)}px;
+`;
+
+export const About = styled.Text`
+    font-family: ${({theme}) => theme.fonts.ArchivoMedium};
+    color: ${({theme}) => theme.colors.subTitle};
+    font-size: ${RFValue(15)}px;
+    text-align: justify;
+
+    margin-top: 24px;
+`
